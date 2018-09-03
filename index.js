ac1a2525f47c80f04b7b9bc6e8dfd3df9fefc39e
Zac Willmington <zac.willmington@gmail.com>
2018-09-03 21:30:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-// Code your solution here!
+function printString(string) {
+  let substring;
+
+  // Print out the current first character in the string.
+  console.log(string[0]);
+
+  // Store the remainder of the string in the 'substring' variable.
+   substring = string.substring(1, string.length);
+  // Invoke printString() from within, passing in the remainder of the previous string.
+	if(substring.length > 1){
+  	     printString(substring);
+	}
+}
+
+printString("pizza");// Code your solution here!
 
